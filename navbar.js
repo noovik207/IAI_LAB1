704ed9dac60a4b62da587d2b9502dc2847193162
noovik207 <nowakkinga207@gmail.com>
2019-10-18 09:22:11 +0200

    nawigacja

---
+++ navbar.js
@@ -0,0 +1,3 @@
+let nav= '<a class="nav-link active" href="index.html">Home</a><a class="nav-link active" href="#">Blog</a><a class="nav-link active" href="#">Quiz</a><a class="nav-link active" href="contact.html">Kontakt</a>'
+let elem = document.querySelector(".navbar");
+elem.innerHTML = nav;
