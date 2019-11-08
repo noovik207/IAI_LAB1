f1ef9157c3fb19d4563ac456cbedd2d892a901ea
Paptryk Ptak <pptaku99@wp.pl>
2019-11-08 09:31:34 +0100

    quiz

--- js/navbar.js
+++ js/navbar.js
@@ -1,3 +1,3 @@
-let nav= '<a class="nav-link active" href="index.html">Home</a><a class="nav-link active" href="#">Blog</a><a class="nav-link active" href="#">Quiz</a><a class="nav-link active" href="contact.html">Kontakt</a>'
+let nav= '<a class="nav-link active" href="index.html">Home</a><a class="nav-link active" href="#">Blog</a><a class="nav-link active" href="quiz.html">Quiz</a><a class="nav-link active" href="contact.html">Kontakt</a>'
 let elem = document.querySelector(".navbar");
 elem.innerHTML = nav;
